1f6c82913de9b01d6c6faaca33114424825b60e1
jarrod10 <jarrodadair43@hotmail.com>
2024-04-09 21:50:57 +1000

    Switched to ms edge. Added new auth functions.

--- src/controllers/user/mediaRequest/mediarequestlibrary.ts
+++ src/controllers/user/mediaRequest/mediarequestlibrary.ts
@@ -2,41 +2,60 @@ import axios, { AxiosResponse, AxiosRequestConfig, RawAxiosRequestHeaders } from
 
 const BASEURL = 'http://139.168.160.38:5055';
 
-const client = axios.create({
-    baseURL: 'https://api.github.com',
-  });
+const client = axios.create();
 
-async function getCurrentAuth() {
-    const requestUrl: string = buildUrl(BASEURL, '/auth/me');
+// async function getCurrentAuth() {
+//     const requestUrl: string = buildUrl(BASEURL, '/login');
+//     const config: AxiosRequestConfig = {
+//         method: 'GET',
+//         headers: {
+//             'Accept': 'application/json'
+//         } as RawAxiosRequestHeaders
+//     };
+//     try {
+//         const response: AxiosResponse = await client.get(requestUrl, config);
+//         return response.data;
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+async function getNewAuth() {
+    const requestUrl: string = buildUrl(BASEURL, '/api/v1/auth/jellyfin');
     const config: AxiosRequestConfig = {
+        method: 'POST',
         headers: {
-        'Accept': 'application/vnd.github+json',
+            'Accept': 'application/json'
         } as RawAxiosRequestHeaders,
+        data: {
+            username: 'Jarrod',
+            password: 'Jacinta123'
+        }
     };
     try {
         const response: AxiosResponse = await client.get(requestUrl, config);
         return response.data;
-    } catch(err) {
+    } catch (err) {
         console.log(err);
-    }  
+    }
 }
 
-function showMediaRequestLibrary(page: string) {
-    return page;
-    // const mediaRequestLibrary = new MediaRequestLibrary();
-    // document.getElementById('main').innerHTML = mediaRequestLibrary.getMediaRequestHTML();
-}
+// function showMediaRequestLibrary(page: string) {
+//     return page;
+//     // const mediaRequestLibrary = new MediaRequestLibrary();
+//     // document.getElementById('main').innerHTML = mediaRequestLibrary.getMediaRequestHTML();
+// }
 
 function buildUrl(base: string, query: string) {
     return base + query;
 }
 
-export default function (view: string) {
+export default async function () { //view: string) {
     // if not already auth'd to Jellyseerr
-    if (!getCurrentAuth()) {
-        // getNewAuth();
-    }
+    await getNewAuth();
+    // if (!getCurrentAuth()) {
+    // }
 
     // getCurrentAuth();
-    // showMediaRequestLibrary(view);
+    // await showMediaRequestLibrary(view);
 }
